2bad979b2b546a12412dc234d2719e460c978b5e
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-30 08:12:18 +0300

    added the landing page

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for photogallery project.
 
@@ -118,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
