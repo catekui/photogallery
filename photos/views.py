5ef02fe18dfe11666c60d42b9ba8ba8d75a71720
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-30 19:18:35 +0300

    added pics and category

--- photos/views.py
+++ photos/views.py
@@ -13,11 +13,16 @@ def index(request):
 
 def gallery(request):
     categories = Category.objects.all()
-    context = {'categories': categories}
+    photos = Photo.objects.all()
+
+
+    context = {'categories': categories, 'photos': photos}
     return render(request,'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
-    return render(request,'photos/photo.html')
+    photos = Photo.objects.get(id=pk)
+    return render(request,'photos/photo.html', {'photo': photo})
+
 
 
 def addPhoto(request):
