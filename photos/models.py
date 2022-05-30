90a3359d3967c8e0c2e9c113218d56e34e42b322
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-30 17:54:34 +0300

    styling of the gallery.html

--- photos/models.py
+++ photos/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-# class Image(models.Model):     
-#     name = models.CharField(max_length=100)
-#     description = models.TextField(max_length=500)
-#     image = models.ImageField(upload_to='ownImages', blank=True)
-#     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
