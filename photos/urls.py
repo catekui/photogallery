a3e7ea682350135a58588febc944c511d9fca904
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-31 13:02:16 +0300

    debugging

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path 
 from . import views
+# from importlib.resources import path, include
 
 
 urlpatterns = [
-   
-    path('', views.gallery),
+    # url(r'^$', views.gallery, name='gallery'),
+    path('', views.gallery, name='gallery'),
     path('index/', views.index, name='index'),
     path('base/', views.base, name='base'),
     path('photo/<str:pk>/', views.viewPhoto, name='photo'),
--- photos/views.py
+++ photos/views.py
@@ -21,15 +21,13 @@ def gallery(request):
 
     categories = Category.objects.all()
     photos = Photo.objects.all()
-
-
     context = {'categories': categories, 'photos': photos}
-    return render(request,'photos/gallery.html', context)
+    return render(request, 'photos/gallery.html' , context)
+
 
 def viewPhoto(request, pk):
-    photos = Photo.objects.get(id=pk)
-    return render(request,'photos/photo.html', {'photo': photo})
-
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html',{'photo': photo} )
 
 
 def addPhoto(request):
